18b31fb1aea43b14f2a19cb0ed683cd09a831a06
Allan Joston Fernandes <allanfernandes459@gmail.com>
2024-02-25 23:07:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import currencyapi from '@everapi/currencyapi-js'
+
+const client = new currencyapi('cur_live_kpmYE8qWrGT8ksGYLUqtOWJvFLaXo767MSyDma9M')
+
+const currencyConverter = async (fromCurrency, toCurrency, units) => {
+    try {
+        const response = await client.latest({
+            base_currency: fromCurrency,
+            currencies: toCurrency
+        });
+
+        if (response.data && response.data[toCurrency]) {
+            const value = await response.data[toCurrency].value;
+            return value * units;
+        } else {
+            throw new Error('Invalid response data');
+        }
+    } catch (error) {
+        throw new Error('Error fetching currency conversion data');
+    }
+}
